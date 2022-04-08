6149211f935af945eed75f7562b90bc42ef8ca30
Subhan Khan <subhank1997@gmail.com>
2022-04-08 15:13:01 -0400

    commit tree b2c2b000bc7e7e6a66601cc28fc9c29021b57ef6

---
+++ index.js
@@ -0,0 +1,38 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+  "Hello!".toUpperCase(); 
+
+
+  function whisper(string) {
+      return string.toLowerCase();
+  }
+  "HELLO!".toLowerCase();
+
+
+  function logShout(string){
+        console.log(string.toUpperCase());    
+  }
+"hello".toUpperCase();
+
+
+function logWhisper(string){
+    console.log(string.toLowerCase());
+}
+"HELLO".toLowerCase();
+
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string === string.toLowerCase()) {
+        return "I can\'t hear you!"
+    }
+    if (string === string.toUpperCase()){
+        return "YES INDEED!"
+    }
+    if (string === "Let\'s have dinner together!") {
+        return "I would love to!"
+    }
+}
+"hello".toLowerCase()
+"HELLO".toUpperCase()
+"Let\'s have dinner together!".string ()    
